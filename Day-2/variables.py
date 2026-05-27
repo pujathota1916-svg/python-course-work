f20080e62bce3ab814ad87615e83de978588c7ec
pujathota1916-svg <pujathota1916@gmail.com>
2026-05-27 17:54:04 +0530

    Add files via upload

---
+++ Day-2/variables.py
@@ -0,0 +1,32 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/thota/Desktop/python course work/Day-2/keyword.py =====
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> 10
+10
+>>> Myvar=10
+>>> Myvar1=10
+>>> Myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> myvar=10
+>>> var=10
+>>> var=20
+>>> var
+20
+>>> var
+20
+>>> a=b=c=10,20,30
+>>> a
+(10, 20, 30)
+>>> b
+(10, 20, 30)
+>>> c
+(10, 20, 30)
